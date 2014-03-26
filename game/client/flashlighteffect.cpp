f0f27bba792e64cd41a4bbb84c491794c59f5bb3
Iván Bravo Bravo <ivan-bravo@infosmart.mx>
2014-03-26 12:26:28 -0600

    Cliente

---
+++ game/client/flashlighteffect.cpp
@@ -0,0 +1,121 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+//===========================================================================//
+
+#include "cbase.h"
+#include "flashlighteffect.h"
+#include "dlight.h"
+#include "iefx.h"
+#include "iviewrender.h"
+#include "view.h"
+#include "engine/ivdebugoverlay.h"
+#include "tier0/vprof.h"
+#include "tier1/KeyValues.h"
+#include "toolframework_client.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+//=========================================================
+// Comandos
+//=========================================================
+
+static ConVar r_flashlightfov( "r_flashlightfov", "50.0", FCVAR_CHEAT );
+static ConVar r_flashlightoffsetx( "r_flashlightoffsetx", "5.0", FCVAR_CHEAT );
+static ConVar r_flashlightoffsety( "r_flashlightoffsety", "-5.0", FCVAR_CHEAT );
+static ConVar r_flashlightoffsetz( "r_flashlightoffsetz", "0.0", FCVAR_CHEAT );
+static ConVar r_flashlightnear( "r_flashlightnear", "2.0", FCVAR_CHEAT );
+static ConVar r_flashlightfar( "r_flashlightfar", "750.0", FCVAR_CHEAT );
+static ConVar r_flashlightconstant( "r_flashlightconstant", "0.0", FCVAR_CHEAT );
+static ConVar r_flashlightlinear( "r_flashlightlinear", "100.0", FCVAR_CHEAT );
+static ConVar r_flashlightquadratic( "r_flashlightquadratic", "0.0", FCVAR_CHEAT );
+static ConVar r_flashlightambient( "r_flashlightambient", "0.0", FCVAR_CHEAT );
+
+//=========================================================
+//=========================================================
+void C_BasePlayer::GetFlashlightOffset( const Vector &vecForward, const Vector &vecRight, const Vector &vecUp, Vector *pVecOffset ) const
+{
+	*pVecOffset = r_flashlightoffsety.GetFloat() * vecUp + r_flashlightoffsetx.GetFloat() * vecRight + r_flashlightoffsetz.GetFloat() * vecForward;
+}
+
+//=========================================================
+// Constructor
+//=========================================================
+CFlashlightEffect::CFlashlightEffect( int nEntIndex ) : CBaseFlashlightEffect( nEntIndex, "effects/flashlight001" )
+{
+}
+
+//=========================================================
+// Inicia la configuración recomendada
+//=========================================================
+void CFlashlightEffect::Init()
+{
+	SetFOV( r_flashlightfov.GetFloat() );
+	SetOffset( r_flashlightoffsetx.GetFloat(), r_flashlightoffsety.GetFloat(), r_flashlightoffsetz.GetFloat() );
+	SetNear( r_flashlightnear.GetFloat() );
+	SetFar( r_flashlightfar.GetFloat() );
+	SetConstant( r_flashlightconstant.GetFloat() );
+	SetLinear( r_flashlightlinear.GetFloat() );
+	SetQuadratic( r_flashlightquadratic.GetFloat() );
+
+	SetBright( 1.0f );
+	SetAlpha( r_flashlightambient.GetFloat() );
+}
+
+CHeadlightEffect::CHeadlightEffect() 
+{
+
+}
+
+CHeadlightEffect::~CHeadlightEffect()
+{
+	
+}
+
+void CHeadlightEffect::UpdateLight( const Vector &vecPos, const Vector &vecDir, const Vector &vecRight, const Vector &vecUp, int nDistance )
+{
+	if ( IsOn() == false )
+		 return;
+
+	FlashlightState_t state;
+	Vector basisX, basisY, basisZ;
+	basisX = vecDir;
+	basisY = vecRight;
+	basisZ = vecUp;
+	VectorNormalize(basisX);
+	VectorNormalize(basisY);
+	VectorNormalize(basisZ);
+
+	BasisToQuaternion( basisX, basisY, basisZ, state.m_quatOrientation );
+		
+	state.m_vecLightOrigin = vecPos;
+
+	state.m_fHorizontalFOVDegrees = 45.0f;
+	state.m_fVerticalFOVDegrees = 30.0f;
+	state.m_fQuadraticAtten = r_flashlightquadratic.GetFloat();
+	state.m_fLinearAtten = r_flashlightlinear.GetFloat();
+	state.m_fConstantAtten = r_flashlightconstant.GetFloat();
+	state.m_Color[0] = 1.0f;
+	state.m_Color[1] = 1.0f;
+	state.m_Color[2] = 1.0f;
+	state.m_Color[3] = r_flashlightambient.GetFloat();
+	state.m_NearZ = r_flashlightnear.GetFloat();
+	state.m_FarZ = r_flashlightfar.GetFloat();
+	state.m_bEnableShadows = true;
+	state.m_pSpotlightTexture = m_nFlashlightTexture;
+	state.m_nSpotlightTextureFrame = 0;
+	
+	if( GetFlashlightHandle() == CLIENTSHADOW_INVALID_HANDLE )
+	{
+		SetFlashlightHandle( g_pClientShadowMgr->CreateFlashlight( state ) );
+	}
+	else
+	{
+		g_pClientShadowMgr->UpdateFlashlightState( GetFlashlightHandle(), state );
+	}
+	
+	g_pClientShadowMgr->UpdateProjectedTexture( GetFlashlightHandle(), true );
+}
+
---
+++ game/client/flashlighteffect.h
@@ -0,0 +1,42 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+//=============================================================================//
+
+#ifndef FLASHLIGHTEFFECT_H
+#define FLASHLIGHTEFFECT_H
+#ifdef _WIN32
+#pragma once
+#endif
+
+struct dlight_t;
+#include "in_flashlighteffect.h"
+
+//=========================================================
+// >> CFlashlightEffect
+//=========================================================
+class CFlashlightEffect : public CBaseFlashlightEffect
+{
+public:
+	DECLARE_CLASS( CFlashlightEffect, CBaseFlashlightEffect );
+
+	CFlashlightEffect( int nEntIndex = 0 );
+
+	virtual void Init();
+};
+
+//=========================================================
+// >> CHeadlightEffect
+//=========================================================
+class CHeadlightEffect : public CFlashlightEffect
+{
+public:
+	
+	CHeadlightEffect();
+	~CHeadlightEffect();
+
+	virtual void UpdateLight(const Vector &vecPos, const Vector &vecDir, const Vector &vecRight, const Vector &vecUp, int nDistance);
+};
+
+#endif // FLASHLIGHTEFFECT_H
